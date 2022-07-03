ed1567fcd144b8b9e00bbf3c0449d6e2f6b870cf
Jlc0m <106530981+Jlc0m@users.noreply.github.com>
2022-07-03 22:27:37 +0300

    add data-base
    This is a database exported from mysql to php-array

---
+++ laravel-example.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.1.1
+ */
+
+/**
+ * Database `laravel-example`
+ */
+
+/* `laravel-example`.`failed_jobs` */
+$failed_jobs = array(
+);
+
+/* `laravel-example`.`migrations` */
+$migrations = array(
+  array('id' => '1','migration' => '2014_10_12_000000_create_users_table','batch' => '1'),
+  array('id' => '2','migration' => '2014_10_12_100000_create_password_resets_table','batch' => '1'),
+  array('id' => '3','migration' => '2019_08_19_000000_create_failed_jobs_table','batch' => '1'),
+  array('id' => '4','migration' => '2019_12_14_000001_create_personal_access_tokens_table','batch' => '1'),
+  array('id' => '5','migration' => '2022_07_02_230548_create_news_table','batch' => '1')
+);
+
+/* `laravel-example`.`news` */
+$news = array(
+  array('id' => '2','author' => 'Boris','title' => 'Laravel Framework','description' => 'How to Properly Install and Use Bootstrap 5 in Laravel 9
+Last updated on: June 1, 2022 by Digamber
+In this tutorial, i am going to show you how to install and use Bootstrap in the Laravel project instantly. If you are new to Laravel and looking to learn how to install Bootstrap 4 in Laravel then you are at the right place we will help you install laravel ui through composer package in less than 5 minutes.','user_id' => '1','created_at' => '2022-07-03 12:51:59','updated_at' => '2022-07-03 14:06:36'),
+  array('id' => '3','author' => 'Dmitry','title' => 'Build your next idea even faster.','description' => 'Beautifully designed, expertly crafted components and templates, built by the makers of Tailwind CSS. The perfect starting point for your next project.','user_id' => '2','created_at' => '2022-07-03 14:23:52','updated_at' => '2022-07-03 14:23:52'),
+  array('id' => '4','author' => 'Kate','title' => 'React JavaScript library for building user interfaces','description' => 'Learn Once, Write Anywhere
+We don’t make assumptions about the rest of your technology stack, so you can develop new features in React without rewriting existing code.
+React can also render on the server using Node and power mobile apps using React Native.','user_id' => '3','created_at' => '2022-07-03 14:26:15','updated_at' => '2022-07-03 14:26:15'),
+  array('id' => '7','author' => 'Alex','title' => 'Build fast, responsive sites with Bootstrap','description' => 'Powerful, extensible, and feature-packed frontend toolkit. Build and customize with Sass, utilize prebuilt grid system and components, and bring projects to life with powerful JavaScript plugins.','user_id' => '4','created_at' => '2022-07-03 15:33:13','updated_at' => '2022-07-03 15:33:13'),
+  array('id' => '10','author' => 'Alex','title' => 'The Progressive JavaScript Framework','description' => 'An approachable, performant and versatile framework for building web user interfaces. An approachable, performant and versatile framework for building web user interfaces.','user_id' => '4','created_at' => '2022-07-03 17:06:46','updated_at' => '2022-07-03 17:07:15')
+);
+
+/* `laravel-example`.`password_resets` */
+$password_resets = array(
+);
+
+/* `laravel-example`.`personal_access_tokens` */
+$personal_access_tokens = array(
+);
+
+/* `laravel-example`.`users` */
+$users = array(
+  array('id' => '1','name' => 'test','email' => 'test@test.com','email_verified_at' => NULL,'password' => '$2y$10$7qftVO91ABa68eOyxctNV.oY1nTmJR4Tg/3z5fxZ3JEWjaeT2LNIm','remember_token' => NULL,'created_at' => '2022-07-03 12:40:27','updated_at' => '2022-07-03 12:40:27'),
+  array('id' => '2','name' => 'Dmitry','email' => 'test@gmail.com','email_verified_at' => NULL,'password' => '$2y$10$EIBsO1Kh/c9VgiXnaJ6nyuymx/fOG1qYwHclfvtwH/9BaBWsJ9seO','remember_token' => NULL,'created_at' => '2022-07-03 14:23:07','updated_at' => '2022-07-03 14:23:07'),
+  array('id' => '3','name' => 'Kate','email' => 'kate@gmail.com','email_verified_at' => NULL,'password' => '$2y$10$oLDn/FjvzCQ6RZ8TSCPi1.JZzj5ai8nfjH3nCoj/ppE1DrcLLm5sC','remember_token' => 'jowWhsJrcEZKKQnM6bfyl02UvWelzEzmwoNUZc9CkmWGEwsnmsm0dAoQ24xB','created_at' => '2022-07-03 14:25:11','updated_at' => '2022-07-03 14:25:11'),
+  array('id' => '4','name' => 'Alex','email' => 'Alex@gmail.com','email_verified_at' => NULL,'password' => '$2y$10$qbnHs2FRP8mkehN9vu3di.xLd5u0iOQsb1uIeN3HYanlkkCzP1fK.','remember_token' => '0SlQtp28r1rjyhwrsX4Uzsyb03ORJCRaTlWaFGSgttYmZsfrG87BegtVj48K','created_at' => '2022-07-03 15:26:26','updated_at' => '2022-07-03 15:26:26')
+);
